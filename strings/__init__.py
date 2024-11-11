45db5caa3ea8acfe84762a988070c44f7feb77a2
istkharalam6202 <istkharalam6202@gmail.com>
2024-11-11 09:46:35 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -3,7 +3,7 @@ from typing import List
 
 import yaml
 
-LOGGERS = "DNS_HACKOP_BOT"  # connect errors api key "Dont change it"
+LOGGERS = "THUNDERKAUNHAI"  # connect errors api key "Dont change it"
 
 languages = {}
 languages_present = {}
