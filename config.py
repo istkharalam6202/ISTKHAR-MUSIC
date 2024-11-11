8f6a6cb0db653ced257c064f9b7cacdd20504e58
istkharalam6202 <istkharalam6202@gmail.com>
2024-11-11 09:43:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Rajputshivsingh65/DNS-MUSIC",
+    "https://github.com/istkharalam6202/ISTKHAR-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
